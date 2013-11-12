6d44509de7820fab7d044a4d253f51ac1167a802
tsantos013 <ajsantos@umass.edu>
2013-11-12 16:28:10 -0500

    transaction_choose_plan edits

--- Query.java
+++ Query.java
@@ -388,12 +388,14 @@ public class Query {
     	_activerentals_count_statement.clearParameters();
     	_activerentals_count_statement.setInt(1, cid);
     	ResultSet count_set = _activerentals_count_statement.executeQuery();
+    	count_set.next();
     	int activeRentals = count_set.getInt(1);
     	
     	//retrieve max allowed rentals for indicated plan
     	_rentals_for_plan_statement.clearParameters();
     	_rentals_for_plan_statement.setInt(1, pid);
     	ResultSet maxRental_set = _rentals_for_plan_statement.executeQuery();
+    	maxRental_set.next();
     	int maxRentals = maxRental_set.getInt(1);
     	
     	//compare active rentals with allowed rentals
@@ -401,7 +403,7 @@ public class Query {
 	_update_rental_plan_statement.clearParameters();
 	_update_rental_plan_statement.setInt(1, pid);
 	_update_rental_plan_statement.setInt(2, cid);
-	_update_rental_plan_statement.executeQuery();
+	_update_rental_plan_statement.executeUpdate();
 	_commit_transaction_statement.executeUpdate();
     }
     else{
